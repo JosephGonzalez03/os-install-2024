107ea96ffc5d8769d3cfdca16783ff5c1550f7b1
Joseph Gonzalez <josephdg3@gmail.com>
2024-01-07 11:20:09 -0600

    Init

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,178 @@
+use clap::{Parser, Subcommand};
+use serde::Deserialize;
+use std::io::stdin;
+use std::process::{Command, Stdio};
+
+#[derive(Deserialize)]
+struct Config {
+    user: String,
+    arch_packages: Vec<String>,
+    eww_repo: String,
+    swww_repo: String,
+    dotfiles_repo: String,
+}
+
+#[derive(Debug, Parser)]
+struct Arguments {
+    #[clap(subcommand)]
+    action: Action,
+}
+
+#[derive(Debug, Subcommand)]
+enum Action {
+    TEST,
+    INSTALL,
+    UNINSTALL,
+}
+
 fn main() {
-    println!("Hello, world!");
+    match Arguments::parse().action {
+        Action::TEST => {
+            println!("installing...");
+            test();
+        }
+        Action::INSTALL => {
+            println!("installing...");
+            install_software()
+        }
+        Action::UNINSTALL => println!("uninstalling..."),
+    }
+}
+
+fn test() {
+    let config: Config = toml::from_str(include_str!("config.toml")).unwrap();
+    let home_path: String = "/home/".to_string() + &config.user;
+    let root_password = String::new();
+    let github_ssh_key: &str = "";
+}
+
+fn install_software() {
+    let config: Config = toml::from_str(include_str!("config.toml")).unwrap();
+    let home_path: String = "/home/".to_string() + &config.user;
+    let github_ssh_key: String = read_password("github ssh key");
+    let root_password: String = read_password("root");
+    let mut git = Command::new("git");
+
+    //install arch packages
+    Command::new("pacman")
+        .args(["-S", config.arch_packages.join(" ").as_str()])
+        .spawn()
+        .unwrap();
+
+    //create apps directory
+    Command::new("mkdir")
+        .args(["-p", &(home_path.clone() + "/apps")])
+        .spawn()
+        .unwrap();
+
+    //create eww binary
+    git.current_dir(home_path.clone() + "/apps")
+        .args(["clone", config.eww_repo.as_str()])
+        .spawn()
+        .unwrap();
+    Command::new("cargo")
+        .current_dir(home_path.clone() + "/apps/eww")
+        .args([
+            "build",
+            "--release",
+            "--no-default-features",
+            "--features=wayland",
+        ])
+        .spawn()
+        .unwrap();
+    Command::new("chmod")
+        .current_dir(home_path.clone() + "/apps/eww/target/release")
+        .args(["+x", "eww"])
+        .spawn()
+        .unwrap();
+    Command::new("sudo ")
+        .current_dir(home_path.clone() + "/apps/eww/target/release")
+        .stdin(Stdio::from(
+            Command::new("echo")
+                .arg(root_password.clone())
+                .stdout(Stdio::piped())
+                .spawn()
+                .unwrap()
+                .stdout
+                .unwrap(),
+        ))
+        .args(["-S", "mv", "eww", "/usr/local/bin/"])
+        .spawn()
+        .unwrap();
+
+    //create swww & swww daemon binaries
+    git.current_dir(home_path.clone() + "/apps")
+        .args(["clone", config.swww_repo.as_str()])
+        .spawn()
+        .unwrap();
+    Command::new("cargo")
+        .current_dir(home_path.clone() + "/apps/swww")
+        .args([
+            "build",
+            "--release",
+            "--no-default-features",
+            "--features=wayland",
+        ])
+        .spawn()
+        .unwrap();
+    Command::new("chmod")
+        .current_dir(home_path.clone() + "/apps/swww/target/release")
+        .args(["+x", "swww swww-daemon"])
+        .spawn()
+        .unwrap();
+    Command::new("sudo")
+        .stdin(Stdio::from(
+            Command::new("echo")
+                .arg(root_password)
+                .stdout(Stdio::piped())
+                .spawn()
+                .unwrap()
+                .stdout
+                .unwrap(),
+        ))
+        .args(["-S", "mv", "swww swww-daemon", "/usr/local/bin/"])
+        .current_dir(home_path.clone() + "/apps/swww/target/release")
+        .spawn()
+        .unwrap();
+
+    //apply dotfiles
+    git.current_dir(home_path.clone() + "/.config")
+        .args([
+            "clone",
+            config
+                .dotfiles_repo
+                .replace("{password}", github_ssh_key.as_str())
+                .as_str(),
+        ])
+        .spawn()
+        .unwrap();
+    let mut stow = Command::new("stow");
+    String::from_utf8(
+        Command::new("ls")
+            .current_dir(home_path.clone() + "/.config/.dotfiles")
+            .output()
+            .unwrap()
+            .stdout,
+    )
+    .unwrap()
+    .split_terminator("\n")
+    .collect::<Vec<&str>>()
+    .into_iter()
+    .filter(|directory_item| !directory_item.contains(".md"))
+    .inspect(|directory| println!("Creating symlink for {}.", directory))
+    .for_each(|directory| {
+        stow.current_dir(home_path.clone() + "/.config/.dotfiles")
+            .arg(directory)
+            .spawn()
+            .unwrap();
+    })
+}
+
+fn read_password(account: &str) -> String {
+    println!("Enter password for {}: ", account);
+    let mut input = String::new();
+    match stdin().read_line(&mut input) {
+        Ok(_) => input,
+        Err(_) => panic!("Undefined behavior. Quiting script."),
+    }
 }
