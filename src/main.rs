2070b8715833855cf61885cda2a856ea6da7c8f3
Joseph Gonzalez <josephdg3@gmail.com>
2024-01-10 20:52:17 -0600

    refactor: Split up install steps

--- src/main.rs
+++ src/main.rs
@@ -21,35 +21,46 @@ struct Arguments {
 
 #[derive(Debug, Subcommand)]
 enum Action {
-    INSTALL,
-    UNINSTALL,
+    #[clap(subcommand)]
+    Install(InstallOption),
+    Uninstall,
+}
+
+#[derive(Debug, Subcommand)]
+enum InstallOption {
+    ArchPackages,
+    Dotfiles,
+    Fonts,
 }
 
 fn main() {
+    let config: Config = toml::from_str(include_str!("config.toml")).unwrap();
+    let home_path: String = "/home/".to_string() + &config.user;
+
     match Arguments::parse().action {
-        Action::INSTALL => {
+        Action::Install(install_option) => {
             println!("installing...");
-            install_software()
+            match install_option {
+                InstallOption::ArchPackages => println!("packages"),
+                InstallOption::Dotfiles => println!("dotfiles"),
+                InstallOption::Fonts => println!("fonts"),
+            }
         }
-        Action::UNINSTALL => println!("uninstalling..."),
+        Action::Uninstall => println!("uninstalling..."),
     }
 }
 
-fn install_software() {
-    let config: Config = toml::from_str(include_str!("config.toml")).unwrap();
-    let home_path: String = "/home/".to_string() + &config.user;
-    let github_ssh_password: String = read_password("github ssh key");
-    let root_password: String = read_password("root");
-    let mut git = Command::new("git");
-
+fn install_fonts(config: Config, home_path: String) {
     let font_name = config
         .font_url
         .split("/")
         .last()
         .unwrap()
         .replace(".zip", "");
+
     println!("Installing font: {}.", font_name);
-    Command::new("touch")
+    println!("{}", home_path.clone() + "/.local/share/fonts");
+    Command::new("mkdir")
         .args(["-p", &(home_path.clone() + "/.local/share/fonts")])
         .spawn()
         .unwrap();
@@ -60,6 +71,7 @@ fn install_software() {
         .unwrap();
     Command::new("unzip")
         .current_dir(home_path.clone() + "/.local/share/fonts")
+        // add font zip to arg list
         .args(["-d", font_name.as_str()])
         .spawn()
         .unwrap();
@@ -71,42 +83,13 @@ fn install_software() {
         ])
         .spawn()
         .unwrap();
+}
+
+fn install_arch_packages(config: Config, home_path: String) {
+    let root_password: String = read_password("root");
 
     println!("Installing Arch packages.");
-    Command::new("pacman")
-        .args(["-S", config.arch_packages.join(" ").as_str()])
-        .spawn()
-        .unwrap();
-
-    //create apps directory
-    Command::new("mkdir")
-        .args(["-p", &(home_path.clone() + "/apps")])
-        .spawn()
-        .unwrap();
-
-    println!("Building eww binary.");
-    git.current_dir(home_path.clone() + "/apps")
-        .args(["clone", config.eww_repo.as_str()])
-        .spawn()
-        .unwrap();
-    Command::new("cargo")
-        .current_dir(home_path.clone() + "/apps/eww")
-        .args([
-            "build",
-            "--release",
-            "--no-default-features",
-            "--features=wayland",
-        ])
-        .spawn()
-        .unwrap();
-    Command::new("chmod")
-        .current_dir(home_path.clone() + "/apps/eww/target/release")
-        .args(["+x", "eww"])
-        .spawn()
-        .unwrap();
-    println!("Installing eww binary.");
     Command::new("sudo ")
-        .current_dir(home_path.clone() + "/apps/eww/target/release")
         .stdin(Stdio::from(
             Command::new("echo")
                 .arg(root_password.clone())
@@ -116,9 +99,93 @@ fn install_software() {
                 .stdout
                 .unwrap(),
         ))
-        .args(["-S", "mv", "eww", "/usr/local/bin/"])
+        .args(["pacman -S", config.arch_packages.join(" ").as_str()])
         .spawn()
         .unwrap();
+}
+
+fn install_dotfiles(config: Config, home_path: String) {
+    let github_ssh_password: String = read_password("github ssh key");
+    println!("Applying personal settings.");
+
+    Command::new("git")
+        .current_dir(home_path.clone() + "/.config")
+        .args([
+            "clone",
+            config
+                .dotfiles_repo
+                .replace("{password}", github_ssh_password.as_str())
+                .as_str(),
+        ])
+        .spawn()
+        .unwrap();
+    let mut stow = Command::new("stow");
+    String::from_utf8(
+        Command::new("ls")
+            .current_dir(home_path.clone() + "/.config/.dotfiles")
+            .output()
+            .unwrap()
+            .stdout,
+    )
+    .unwrap()
+    .split_terminator("\n")
+    .collect::<Vec<&str>>()
+    .into_iter()
+    .filter(|directory_item| !directory_item.contains(".md"))
+    .inspect(|directory| println!("Creating symlink for {}.", directory))
+    .for_each(|directory| {
+        stow.current_dir(home_path.clone() + "/.config/.dotfiles")
+            .arg(directory)
+            .spawn()
+            .unwrap();
+    })
+}
+
+fn install_software(config: Config, home_path: String) {
+    let root_password: String = read_password("root");
+    let mut git = Command::new("git");
+
+    //create apps directory
+    Command::new("mkdir")
+        .args(["-p", &(home_path.clone() + "/apps")])
+        .spawn()
+        .unwrap();
+
+    //println!("Building eww binary.");
+    //git.current_dir(home_path.clone() + "/apps")
+    //    .args(["clone", config.eww_repo.as_str()])
+    //    .spawn()
+    //    .unwrap();
+    //Command::new("cargo")
+    //    .current_dir(home_path.clone() + "/apps/eww")
+    //    .args([
+    //        "build",
+    //        "--release",
+    //        "--no-default-features",
+    //        "--features=wayland",
+    //    ])
+    //    .spawn()
+    //    .unwrap();
+    //Command::new("chmod")
+    //    .current_dir(home_path.clone() + "/apps/eww/target/release")
+    //    .args(["+x", "eww"])
+    //    .spawn()
+    //    .unwrap();
+    //println!("Installing eww binary.");
+    //Command::new("sudo ")
+    //    .current_dir(home_path.clone() + "/apps/eww/target/release")
+    //    .stdin(Stdio::from(
+    //        Command::new("echo")
+    //            .arg(root_password.clone())
+    //            .stdout(Stdio::piped())
+    //            .spawn()
+    //            .unwrap()
+    //            .stdout
+    //            .unwrap(),
+    //    ))
+    //    .args(["-S", "mv", "eww", "/usr/local/bin/"])
+    //    .spawn()
+    //    .unwrap();
 
     println!("Building swww and swww-daemon binaries.");
     git.current_dir(home_path.clone() + "/apps")
@@ -155,38 +222,6 @@ fn install_software() {
         .current_dir(home_path.clone() + "/apps/swww/target/release")
         .spawn()
         .unwrap();
-
-    println!("Applying personal settings.");
-    git.current_dir(home_path.clone() + "/.config")
-        .args([
-            "clone",
-            config
-                .dotfiles_repo
-                .replace("{password}", github_ssh_password.as_str())
-                .as_str(),
-        ])
-        .spawn()
-        .unwrap();
-    let mut stow = Command::new("stow");
-    String::from_utf8(
-        Command::new("ls")
-            .current_dir(home_path.clone() + "/.config/.dotfiles")
-            .output()
-            .unwrap()
-            .stdout,
-    )
-    .unwrap()
-    .split_terminator("\n")
-    .collect::<Vec<&str>>()
-    .into_iter()
-    .filter(|directory_item| !directory_item.contains(".md"))
-    .inspect(|directory| println!("Creating symlink for {}.", directory))
-    .for_each(|directory| {
-        stow.current_dir(home_path.clone() + "/.config/.dotfiles")
-            .arg(directory)
-            .spawn()
-            .unwrap();
-    })
 }
 
 fn read_password(account: &str) -> String {
